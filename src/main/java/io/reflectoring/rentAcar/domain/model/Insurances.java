e592d1a381e6fc6a17ade9b285fb10cfda28332e
AbdullahKarakoc <abdullahkrkc1453@gmail.com>
2024-07-01 13:39:34 +0300

    feat: add Insurances entity

--- src/main/java/io/reflectoring/rentAcar/domain/model/Insurances.java
+++ src/main/java/io/reflectoring/rentAcar/domain/model/Insurances.java
@@ -1,17 +1,23 @@
 package io.reflectoring.rentAcar.domain.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import io.reflectoring.rentAcar.enums.InsuranceCategory;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.UuidGenerator;
 import org.hibernate.annotations.Where;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @Data
@@ -27,5 +33,40 @@ public class Insurances {
     @Id
     @UuidGenerator(style = UuidGenerator.Style.RANDOM)
     private UUID id;
-    private
+    private String provider;
+    private LocalDateTime start;
+    private LocalDateTime end;
+    private int cost;
+    private InsuranceCategory insuranceCategory;
+
+
+    @JsonManagedReference
+    @OneToMany
+    @JoinColumn(name = "insurance_id")
+    private List<Insurances> insurances;
+
+
+
+    @CreatedDate
+    @Column(
+            nullable = false,
+            updatable = false
+    )
+    private LocalDateTime createDate;
+
+    @LastModifiedDate
+    @Column(insertable = false)
+    private LocalDateTime lastModified;
+
+    @CreatedBy
+    @Column(
+            nullable = false,
+            updatable = false
+    )
+    private String createBy;
+
+    @LastModifiedBy
+    @Column(insertable = false)
+    private String lastModifiedBy;
+
 }
