06daec371dde7665afa0cafc11a92f6b0b186f5a
AbdullahKarakoc <abdullahkrkc1453@gmail.com>
2024-07-05 12:05:13 +0300

    feat: add customers and rentals service

--- src/main/java/io/reflectoring/rentAcar/controller/CustomersController.java
+++ src/main/java/io/reflectoring/rentAcar/controller/CustomersController.java
@@ -1,4 +1,42 @@
 package io.reflectoring.rentAcar.controller;
 
+import io.reflectoring.rentAcar.domain.request.CustomersRequestDto;
+import io.reflectoring.rentAcar.domain.response.CustomersResponseDto;
+import io.reflectoring.rentAcar.service.CustomersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/customers")
 public class CustomersController {
+    @Autowired
+    private CustomersService customerService;
+
+    @GetMapping
+    public List<CustomersResponseDto> getAllCustomers() {
+        return customerService.getAllCustomers();
+    }
+
+    @GetMapping("/{id}")
+    public CustomersResponseDto getCustomerById(@PathVariable UUID id) {
+        return customerService.getCustomerById(id);
+    }
+
+    @PostMapping
+    public CustomersResponseDto saveCustomer(@RequestBody CustomersRequestDto customerRequestDto) {
+        return customerService.saveCustomer(customerRequestDto);
+    }
+
+    @PutMapping("/{id}")
+    public CustomersResponseDto updateCustomer(@PathVariable UUID id, @RequestBody CustomersRequestDto customerRequestDto) {
+        return customerService.updateCustomer(id, customerRequestDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCustomer(@PathVariable UUID id) {
+        customerService.deleteCustomer(id);
+    }
 }
--- src/main/java/io/reflectoring/rentAcar/controller/RentalsController.java
+++ src/main/java/io/reflectoring/rentAcar/controller/RentalsController.java
@@ -1,4 +1,42 @@
 package io.reflectoring.rentAcar.controller;
 
+import io.reflectoring.rentAcar.domain.request.RentalsRequestDto;
+import io.reflectoring.rentAcar.domain.response.RentalsResponseDto;
+import io.reflectoring.rentAcar.service.RentalsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/rentals")
 public class RentalsController {
+    @Autowired
+    private RentalsService rentalService;
+
+    @GetMapping
+    public List<RentalsResponseDto> getAllRentals() {
+        return rentalService.getAllRentals();
+    }
+
+    @GetMapping("/{id}")
+    public RentalsResponseDto getRentalById(@PathVariable UUID id) {
+        return rentalService.getRentalById(id);
+    }
+
+    @PostMapping
+    public RentalsResponseDto saveRental(@RequestBody RentalsRequestDto rentalRequestDto) {
+        return rentalService.saveRental(rentalRequestDto);
+    }
+
+    @PutMapping("/{id}")
+    public RentalsResponseDto updateRental(@PathVariable UUID id, @RequestBody RentalsRequestDto rentalRequestDto) {
+        return rentalService.updateRental(id, rentalRequestDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteRental(@PathVariable UUID id) {
+        rentalService.deleteRental(id);
+    }
 }
